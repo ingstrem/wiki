26332f05199dfbe6ee8b6da62d089b2bd90557af
Ilya Dobatovkin <ramon_mattlov@vulong-development.ru>
2025-02-27 17:09:33 +0000

    chore: added styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 
   // eleventyConfig.addPassthroughCopy('src/css')
+  eleventyConfig.addPassthroughCopy({ static: '/' });
 
   return {
     passthroughFileCopy: true,
