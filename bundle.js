ad902bd7c4c2bc05fc66a4e5269b7046de888fcc
kudgar <kudgar@users.noreply.github.com>
2026-01-05 09:10:30 +0000

    deploy: 073358b0bc42ee82ecc3774418870bdd66c77498

--- bundle.js
+++ bundle.js
@@ -1,1 +1,6 @@
 const loc = location.pathname;
+const links = document.querySelectorAll(".header-link");
+
+links.forEach((link) => {
+  if (link.getAttribute("href") === loc) link.classList.add("active");
+});
