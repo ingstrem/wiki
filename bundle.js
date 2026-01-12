e471dfe525648aa2e6ad289a0db9db9ee36a67bd
kudgar <kudgar@users.noreply.github.com>
2026-01-12 01:20:02 +0000

    deploy: a36dc487646f0dd69a0fff0f384d475d3d1e81f8

--- bundle.js
+++ bundle.js
@@ -4,3 +4,32 @@ const links = document.querySelectorAll(".header-link");
 links.forEach((link) => {
   if (link.getAttribute("href") === loc) link.classList.add("active");
 });
+
+// Стилизация коментариев
+document.querySelectorAll("code").forEach((codeBlock) => {
+  let lines = codeBlock.textContent.split("\n");
+
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (line.includes("$")) {
+      let parts = line.split("$", 2);
+
+      if (parts.length > 1 && parts[1].trim() !== "") {
+        lines[
+          i
+        ] = `${parts[0]}<span class="terminal-color">$ ${parts[1]}</span>`;
+      }
+    }
+    if (line.includes("#")) {
+      let parts = line.split("#", 2);
+
+      if (parts.length > 1 && parts[1].trim() !== "") {
+        lines[
+          i
+        ] = `${parts[0]}<span class="comment-color"># ${parts[1]}</span>`;
+      }
+    }
+  }
+
+  codeBlock.innerHTML = lines.join("<br>");
+});
